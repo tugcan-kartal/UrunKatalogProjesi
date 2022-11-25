522c954a2f8b0ed0a6dac9dd6a1818bf93b0c949
tugcan-kartal <tugcankartal01@gmail.com>
2022-11-25 17:16:22 +0300

    altıncı gonderme

--- src/components/ItemsPage.js
+++ src/components/ItemsPage.js
@@ -5,14 +5,13 @@ import './ItemsPage.css';
 function ItemsPage() {
 
     const [infos,setInfos]=useState([]);
-
+    
     useEffect(()=>{
         axios.get("https://assignment-api.piton.com.tr/api/v1/product/all",{
           headers:{
             "access-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IiIsImlhdCI6MTY1MDE4OTM0NywiZXhwIjoxNjc2MTA5MzQ3fQ.r7j-guzdX9M9LVaxzwGtLvACwI3HbfbGkOj-QbHKRJo"
           }
         }).then((response)=>{
-          console.log(response.data.products)
             setInfos(response.data.products);
         }).catch(()=>{
             console.log("Err");
@@ -21,13 +20,34 @@ function ItemsPage() {
 
   return (
     <div>
-        <div className='text-red-400'>
+        <div>
           {infos?.map((val)=>{
             return (
-              <div key={val.id} className='friendContainer'>
-                <div className='friend'> 
+              <div key={val.id}>
+
+                <div> 
                   <h3>Name: {val.name}</h3> 
+                  <h5>Price: {val.price}</h5>
                 </div>
+
+                <div>
+                  <img src='https://www.atap.com.tr/storage/images/32b84246280a488fa70b9933f3c42647.jpg' ></img>
+                </div>
+
+                <div>Number of likes {val.likes} </div>
+
+                <div> 
+                  <label>Saved in favourite</label>
+                  <input type='checkbox'></input> 
+                </div>
+
+                <div className='hidden'>
+                  <div>{val.name}</div>
+                  <div>{val.description}</div>
+                  <div>{val.price}</div>
+                  <div> <img src='https://www.atap.com.tr/storage/images/32b84246280a488fa70b9933f3c42647.jpg' ></img> </div>
+                </div>
+
               </div>  
             )
           })}
